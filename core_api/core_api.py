0c47f8bf2ca99a4ac2f0c31e10c8fd7aac7d509c
Lucas Thimoteo <lucas.thimoteo@radixeng.com.br>
2020-01-10 09:39:44 -0300

    Method abstraction in CoreAPI class

--- core_api/core_api.py
+++ core_api/core_api.py
@@ -5,6 +5,7 @@ from os import listdir,getcwd
 from os.path import isfile, join
 import yaml
 
+from pprint import pprint
 class CoreApi:
 
     def __init__(self, environment):
@@ -16,6 +17,7 @@ class CoreApi:
         solution_result = system_core.find_by_id(solution['id'])    
         if len(solution_result.content) == 0:
             solution_result = system_core.create(solution)
+        pprint(solution_result.content)
         return solution_result.content
 
     def register_app(self, app):
@@ -23,23 +25,26 @@ class CoreApi:
         app_result = system_core.find_by_id(app['id'])      
         if len(app_result.content) == 0:
             app_result = system_core.create(app)
+        pprint(app_result.content)
         return app_result.content
 
     def upload_maps(self, solution,app):
         path = getcwd() + '/Mapa/'
-        map_names = [f for f in listdir(path) if isfile(join(path, f)) and (f.endswith('.yaml') or f.endswith('.yml'))]
+        map_names = self.__list_yaml_files(path)
         map_yml = self.__get_files(map_names,path)[0]
         map_yml['systemId'] = solution['id']
         map_yml['processId'] = app['id']
 
         system_core = SystemCore(self.url,'map')
         map_result = system_core.create(map_yml)
+        pprint(map_result.content)
+
         return map_result.content
 
     def upload_operations(self,solution,app):
         
         path = getcwd() + '/Metadados/'
-        metadata_names = [f for f in listdir(path) if isfile(join(path, f)) and (f.endswith('.yaml') or f.endswith('.yml'))]
+        metadata_names = self.__list_yaml_files(path)
         metadata_yml = self.__get_files(metadata_names,path)[0]
         operations = []
         for operation in metadata_yml['operations']:
@@ -51,8 +56,12 @@ class CoreApi:
             operations.append(operation)
         system_core = SystemCore(self.url,'operation')
         operations_result = system_core.create(operations)
+        pprint(operations_result.content)
         return operations_result.content
         
+    def __list_yaml_files(self,path):
+        return [f for f in listdir(path) if isfile(join(path, f)) and (f.endswith('.yaml') or f.endswith('.yml'))]
+
     def __get_files(self,file_names,folder):
         file_list = []
         for file in file_names:
