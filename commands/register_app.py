6d67bb8f603b02c4014fbb52c4eb2a1d7934bc46
Lucas Thimoteo <lucas.thimoteo@radixeng.com.br>
2020-01-15 19:14:20 -0300

    Fixing map creation and formatting methods with pep8

--- commands/register_app.py
+++ commands/register_app.py
@@ -3,40 +3,43 @@ from domain.domain_schema import DomainSchema
 from core_api.core_api import CoreApi
 
 from pprint import pprint
+
+
 class RegisterApp(PlatformBase):
 
     def __init__(self, args):
         super().__init__(args)
         self.solution = self.platform_json['solution']
         self.app = self.platform_json['app']
-        
+
         self.schema = DomainSchema(args.environment)
         self.core_api = CoreApi(args.environment)
 
     def register(self):
-        self.__register_domain_schema(self.solution.copy(),self.app.copy())
-        self.__register_core_api(self.solution.copy(),self.app.copy())
+        self.__register_domain_schema(self.solution.copy(), self.app.copy())
+        self.__register_core_api(self.solution.copy(), self.app.copy())
 
-    def __register_domain_schema(self,solution,app):
+    def __register_domain_schema(self, solution, app):
         self.schema.create_solution(solution)
         print('Solution Created')
 
         app_id = self.schema.get_app_id(solution['id_domain'], app['name'])
         if app_id:
-            self.schema.update_app(app_id, app, solution, self.get_app_and_tag())
+            self.schema.update_app(app_id, app, solution,
+                                   self.get_app_and_tag())
             print('App Created')
         else:
             self.schema.create_app(app, solution, self.get_app_and_tag())
             print('App Updated')
 
-    def __register_core_api(self,solution,app):
+    def __register_core_api(self, solution, app):
         self.core_api.register_solution(solution)
         print('Registered Solution in Core Api')
         self.core_api.register_app(app)
         print('Registered Application in Core Api')
 
-        self.core_api.upload_maps(solution,app)
+        self.core_api.upload_maps(solution, app)
         print('Maps Uploaded in Core Api')
 
-        self.core_api.upload_operations(solution,app)
+        self.core_api.upload_operations(solution, app)
         print('Metadata Uploaded in Core Api')
--- core_api/core_api.py
+++ core_api/core_api.py
@@ -1,69 +1,65 @@
 from platform_sdk.core_api import SystemCore
 import settings
 
-from os import listdir,getcwd
+from os import listdir, getcwd
 from os.path import isfile, join
 import yaml
 
-from pprint import pprint
+
 class CoreApi:
 
     def __init__(self, environment):
         self.environment = environment
         self.url = settings.CORE_API[environment]['uri']
 
-    def register_solution(self,solution):
-        system_core = SystemCore(self.url,'system')
-        solution_result = system_core.find_by_id(solution['id'])    
+    def register_solution(self, solution):
+        system_core = SystemCore(self.url, 'system')
+        solution_result = system_core.find_by_id(solution['id'])
         if len(solution_result.content) == 0:
             solution_result = system_core.create(solution)
         return solution_result.content
 
     def register_app(self, app):
-        system_core = SystemCore(self.url,'installedApp')
-        app_result = system_core.find_by_id(app['id'])      
+        system_core = SystemCore(self.url, 'installedApp')
+        app_result = system_core.find_by_id(app['id'])
         if len(app_result.content) == 0:
             app_result = system_core.create(app)
         return app_result.content
 
-    def upload_maps(self, solution,app):
+    def upload_maps(self, solution, app):
         path = getcwd() + '/Mapa/'
         map_names = self.__list_yaml_files(path)
-        map_yml = self.__get_files(map_names,path)[0]
-        map_yml['systemId'] = solution['id']
-        map_yml['processId'] = app['id']
-        # map_yml['reprocess'] = True
-        breakpoint()
-
-        system_core = SystemCore(self.url,'map')
-        map_result = system_core.create(map_yml)
-        pprint(map_result.content)
-
+        map_yml = self.__get_files(map_names, path)[0]
+        maps = []
+        for m in map_yml:
+            map_yml[m]['systemId'] = solution['id']
+            map_yml[m]['processId'] = app['id']
+            maps.append(map_yml[m])
+        system_core = SystemCore(self.url, 'map')
+        map_result = system_core.create(maps)
         return map_result.content
 
-    def upload_operations(self,solution,app):
-        
+    def upload_operations(self, solution, app):
+
         path = getcwd() + '/Metadados/'
         metadata_names = self.__list_yaml_files(path)
-        metadata_yml = self.__get_files(metadata_names,path)[0]
+        metadata_yml = self.__get_files(metadata_names, path)[0]
         operations = []
         for operation in metadata_yml['operations']:
-            
+
             operation['systemId'] = solution['id']
             operation['processId'] = app['id']
             operation['event_in'] = operation['event']
             operation['event_out'] = operation['name'] + '.done'
             operations.append(operation)
-        system_core = SystemCore(self.url,'operation')
-        breakpoint()
+        system_core = SystemCore(self.url, 'operation')
         operations_result = system_core.create(operations)
-        # pprint(operations_result.content)
         return operations_result.content
-        
-    def __list_yaml_files(self,path):
+
+    def __list_yaml_files(self, path):
         return [f for f in listdir(path) if isfile(join(path, f)) and (f.endswith('.yaml') or f.endswith('.yml'))]
 
-    def __get_files(self,file_names,folder):
+    def __get_files(self, file_names, folder):
         file_list = []
         for file in file_names:
             with open(folder+file, 'r') as stream:
